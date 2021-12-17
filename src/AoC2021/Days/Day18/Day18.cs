e159e5d72a15fc21e62ff63a68e590000e8f57e4
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2021-12-17 18:52:54 +0000

    Add day 18 solution.

---
+++ src/AoC2021/Days/Day18/Day18.cs
@@ -0,0 +1,31 @@
+using System.IO;
+
+namespace AoC2021.Days
+{
+    public class Day18 : IDay
+    {
+        private string[] input;
+
+        public Day18(string file)
+        {
+            LoadInput(file);
+        }
+
+        private void LoadInput(string file)
+        {
+            input = File.ReadAllLines(file);
+        }
+
+        public string PartOne()
+        {
+            var answer = 0;
+            return answer.ToString();
+        }
+
+        public string PartTwo()
+        {
+            var answer = 0;
+            return answer.ToString();
+        }
+    }
+}
---
+++ test/AoC2021.Tests/Days/Day18Tests.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Reflection;
+using Xunit;
+using AoC2021.Days;
+
+namespace AoC2021.Tests.Days
+{
+    public class Day18Tests
+    {
+        [Fact]
+        public void Day18Test1()
+        {
+            // arrange
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path = Path.GetFullPath(Path.Combine(path, "..", "..", ".."));
+
+            var inputFile = Directory.GetFiles(path + @"/TestInput", "Day18_1.txt")[0];
+            var day18 = new Day18(inputFile);
+
+            // act
+            var result1 = day18.PartOne();
+            var result2 = day18.PartTwo();
+
+            // assert
+            Assert.Equal("0", result1);
+            Assert.Equal("0", result2);
+        }
+    }
+}
