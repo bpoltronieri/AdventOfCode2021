65e8280d9b029d0638a2b3cc08b5df9f6d64c15d
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2021-12-21 23:32:25 +0000

    Faster recursive solution to 21 part 2.

--- src/AoC2021/Days/Day21/Day21.cs
+++ src/AoC2021/Days/Day21/Day21.cs
@@ -66,48 +66,25 @@ namespace AoC2021.Days
             }
         }
 
+        public string PartTwo()
+        {
+            var cache = new Dictionary<GameState, (long, long)>(); // maps game state to eventual number of wins for each player
+            var startState = new GameState(player1Start, player2Start, 0, 0, true);
+            var (player1Wins, player2Wins) = GetWinsFromGameState(startState, cache);
+            return Math.Max(player1Wins, player2Wins).ToString();
+        }
+
         // In part two, each turn, we are interested in the sum of 3 rolls of the
         // quantum die. there are 27 possible outcomes but only 7 distinct sums
         private Dictionary<int, int> quantumDieSums = new Dictionary<int, int>() { {3, 1}, {4, 3}, {5, 6}, {6, 7}, {7, 6}, {8, 3}, {9, 1} };
 
-        public string PartTwo()
+        private (long, long) GetWinsFromGameState(GameState game, Dictionary<GameState, (long, long)> cache)
         {
-            var universes = new Dictionary<GameState, long>();
-            var startState = new GameState(player1Start, player2Start, 0, 0, true);
-            universes[startState] = 1;
+            // given a game state, returns the number of eventual wins for each player
+            if (cache.ContainsKey(game))
+                return cache[game];
 
-            long player1WinUniverses = 0;
-            long player2WinUniverses = 0;
-
-            while (universes.Count() > 0)
-            {
-                // Console.WriteLine("Number of Distinct Universes " + universes.Count().ToString());
-                
-                var newUniverses = new Dictionary<GameState, long>();
-                foreach (var game in universes.Keys)
-                {
-                    var count = universes[game];
-                    if (game.GameOver())
-                        UpdateWinCounts(ref player1WinUniverses, ref player2WinUniverses, game, count);
-                    else
-                        SplitGameIntoNewUniverses(newUniverses, game, count);
-                }
-                universes = newUniverses;
-            }
-
-            return Math.Max(player1WinUniverses, player2WinUniverses).ToString();
-        }
-
-        private void UpdateWinCounts(ref long player1WinUniverses, ref long player2WinUniverses, GameState game, long count)
-        {
-            if (game.Winner() == 1)
-                player1WinUniverses += count;
-            else if (game.Winner() == 2)
-                player2WinUniverses += count;
-        }
-
-        private void SplitGameIntoNewUniverses(Dictionary<GameState, long> newUniverses, GameState game, long count)
-        {
+            (long, long) totalWins = (0, 0);
             foreach (var rollSum in quantumDieSums.Keys)
             {
                 GameState newGameState;
@@ -116,33 +93,31 @@ namespace AoC2021.Days
                     var newPlayer1Square = (game.Player1Square + rollSum - 1) % 10 + 1;
                     var newPlayer1Score = game.Player1Score + newPlayer1Square;
                     newGameState = new GameState(newPlayer1Square, game.Player2Square,
-                                                            newPlayer1Score, game.Player2Score,
-                                                            false);
+                                                 newPlayer1Score, game.Player2Score,
+                                                 false);
                 }
                 else
                 {
                     var newPlayer2Square = (game.Player2Square + rollSum - 1) % 10 + 1;
                     var newPlayer2Score = game.Player2Score + newPlayer2Square;
                     newGameState = new GameState(game.Player1Square, newPlayer2Square,
-                                                            game.Player1Score, newPlayer2Score,
-                                                            true);
+                                                 game.Player1Score, newPlayer2Score,
+                                                 true);
                 }
-                IncreaseCount(newUniverses, newGameState, count * quantumDieSums[rollSum]);
+
+                (long, long) wins;
+                if (newGameState.GameOver())
+                    wins = newGameState.Winner() == 1 ? (1, 0) : (0, 1);
+                else
+                    wins = GetWinsFromGameState(newGameState, cache);
+
+                totalWins.Item1 += quantumDieSums[rollSum] * wins.Item1;
+                totalWins.Item2 += quantumDieSums[rollSum] * wins.Item2;
             }
+
+            cache[game] = totalWins;
+            return totalWins;
         }
 
-        private void IncreaseCount(Dictionary<GameState, long> universes, GameState gameState, long value)
-        {
-            if (universes.ContainsKey(gameState))
-                universes[gameState] += value;
-            else
-                universes[gameState] = value;
-        }
-
-        private bool GamesAllDone(Dictionary<GameState, long> universes)
-        {
-            return universes.Where(x => !x.Key.GameOver())
-                            .All(x => x.Value == 0);
-        }
     }
 }
