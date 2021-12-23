cb78cdf52e0ae04ed82af2861db3b221ce16b035
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2021-12-23 02:25:39 +0000

    Add empty day 23 solution.

---
+++ test/AoC2021.Tests/Days/Day23Tests.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Reflection;
+using Xunit;
+using AoC2021.Days;
+
+namespace AoC2021.Tests.Days
+{
+    public class Day23Tests
+    {
+        [Fact]
+        public void Day23Test1()
+        {
+            // arrange
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path = Path.GetFullPath(Path.Combine(path, "..", "..", ".."));
+
+            var inputFile = Directory.GetFiles(path + @"/TestInput", "Day23_1.txt")[0];
+            var day23 = new Day23(inputFile);
+
+            // act
+            var result1 = day23.PartOne();
+            var result2 = day23.PartTwo();
+
+            // assert
+            Assert.Equal("0", result1);
+            Assert.Equal("0", result2);
+        }
+    }
+}
