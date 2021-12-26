8db8777a305c316d78d7be5c43332dcb03e10fc9
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2021-12-26 17:00:51 +0000

    Add visualisation for day 25.

--- src/AoC2021/Days/Day25/Day25.cs
+++ src/AoC2021/Days/Day25/Day25.cs
@@ -1,4 +1,9 @@
 using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using System;
+using System.Windows;
 
 namespace AoC2021.Days
 {
@@ -20,13 +25,20 @@ namespace AoC2021.Days
                     inputMap[x,y] = input[y][x];
         }
 
+        private char[,] CopyMap(char[,] map)
+        {
+            return (char[,])map.Clone();
+        }
+
         public string PartOne()
         {
             var moved = true;
             var map = CopyMap(inputMap);
             var nSteps = 0;
+            // var bitmapList = new List<Bitmap>(); // for visualisation
             while (moved)
             {
+                // bitmapList.Add(CreateBitmap(map));
                 moved = false;
                 // east facing first
                 var newMap = CopyMap(map);
@@ -58,12 +70,8 @@ namespace AoC2021.Days
                 
                 nSteps += 1;
             }
+            // CreateAndSaveGif(bitmapList);
             return nSteps.ToString();
-        }
-
-        private char[,] CopyMap(char[,] map)
-        {
-            return (char[,])map.Clone();
         }
 
         public string PartTwo()
@@ -71,6 +79,35 @@ namespace AoC2021.Days
             // no part two on day 25!
             var answer = 0;
             return answer.ToString();
+        }
+
+        // visualisation
+        private Dictionary<char, Color> colours = new Dictionary<char, Color>() {{'.', Color.White}, {'>', Color.Red}, {'v', Color.Magenta}};
+        private Bitmap CreateBitmap(char[,] map)
+        {
+            var bmp = new Bitmap(map.GetLength(0), map.GetLength(1));
+            for (var x = 0; x < map.GetLength(0); x++)
+                for (var y = 0; y < map.GetLength(1); y++)
+                    bmp.SetPixel(x, y, colours[map[x,y]]);
+            // bmp = new Bitmap(bmp, new System.Drawing.Size(bmp.Width * 3, bmp.Height * 3)); // to scale up
+            return bmp;
+        }
+        private void CreateAndSaveGif(List<Bitmap> bitmapList)
+        {
+            System.Windows.Media.Imaging.GifBitmapEncoder gEnc = new GifBitmapEncoder();
+
+            foreach (System.Drawing.Bitmap bmpImage in bitmapList)
+            {
+                var bmp = bmpImage.GetHbitmap();
+                var src = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    bmp,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                gEnc.Frames.Add(BitmapFrame.Create(src));
+            }
+            using(FileStream fs = new FileStream("Visualisations/25_1.gif", FileMode.Create))
+                gEnc.Save(fs);
         }
     }
 }
